b85aa2b92daf2e66dc5d4afdaddf0060c16b6cd3
Xinling912 <2241649635@qq.com>
2026-03-17 18:42:40 +0800

    chore: untrack .idea files and apply gitignore

--- src/main/java/com/cpt202/app/controller/BookingController.java
+++ src/main/java/com/cpt202/app/controller/BookingController.java
@@ -1,4 +1,23 @@
 package com.cpt202.app.controller;
 
+import com.cpt202.app.model.Booking;
+import com.cpt202.app.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BookingController {
+    @Autowired
+    private BookingRepository bookingRepository;//用于操作数据库的实例
+
+    @GetMapping("/getbookings")
+    public String getBooking(){
+        return "hello,hello";
+    }
+
+
+
 }
--- src/main/java/com/cpt202/app/repository/BookingRepository.java
+++ src/main/java/com/cpt202/app/repository/BookingRepository.java
@@ -1,4 +1,7 @@
 package com.cpt202.app.repository;
 
-public interface BookingRepository {
+import com.cpt202.app.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking,Long> {
 }
