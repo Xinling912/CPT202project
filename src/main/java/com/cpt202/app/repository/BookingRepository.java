3940d5dd39af448b2e51006cd5b9b8741d3cf3e1
Xinling912 <2241649635@qq.com>
2026-04-19 13:11:39 +0800

    成功合并了Selene，Grace和Shaohui支

--- src/main/java/com/cpt202/app/repository/BookingRepository.java
+++ src/main/java/com/cpt202/app/repository/BookingRepository.java
@@ -32,14 +32,15 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
             List<BookingStatus> activeStatuses
     );
 
-    // Spring Data JPA 会根据方法名自动生成 SQL，不需要手写
+    //
     long countByCustomerIdAndStatusAndCreatedAtAfter(
             Long customerId,
             BookingStatus status,
             LocalDateTime date);
 
-    // 进阶魔法：查专家ID，并且订单状态必须在我们给定的集合(In)里面
+    // 查专家ID，并且订单状态必须在我们给定的集合(In)里面
     List<Booking> findByTimeSlot_Specialist_IdAndStatusIn(Long specialistId, List<BookingStatus> statuses);
+
     // 根据专家的 ID 和 订单状态 查询所有有效订单
     List<Booking> findBySpecialistIdAndStatusIn(Long specialistId, List<BookingStatus> statuses);
 
